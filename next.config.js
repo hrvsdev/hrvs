b57c517b1b0c96d1b9e27a0433aa50d010ae4192
kingvyas <kumarvyasss@gmail.com>
2022-10-18 22:27:38 +0530

    added permanet redirects for various sites

--- next.config.js
+++ next.config.js
@@ -2,6 +2,61 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  redirects: async () => {
+    return [
+      {
+        source: "/gt",
+        destination: "https://github.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/gt/:path*",
+        destination: "https://github.com/hrvsdev/:path*",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/github/:path*",
+        destination: "https://github.com/hrvsdev/:path*",
+        permanent: true,
+      },
+      {
+        source: "/mail",
+        destination: "mailto:hrvsdev@gmail.com",
+        permanent: true,
+      },
+      {
+        source: "/tw",
+        destination: "https://twitter.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/ig",
+        destination: "https://instagram.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/insta",
+        destination: "https://instagram.com/hrvsdev",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://instagram.com/hrvsdev",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
