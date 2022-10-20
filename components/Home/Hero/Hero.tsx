6ca72275189df33cb9cf7c96e11abe012c29485b
kingvyas <kumarvyasss@gmail.com>
2022-10-20 12:43:59 +0530

    added svg loader and changed about text

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,15 +4,15 @@ export default function HeroComp(): JSX.Element {
       <div className="flex flex-col max-w-screen-lg w-full">
         <p className="text-lg sm:text-xl text-gray-500 pl-1 mb-1">Hi there, I&apos;m</p>
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-2 sm:mb-4">
-          Harsh Vyas.
+          Harsh Vyas
         </h1>
         <p className="text-4xl sm:text-5xl md:text-6xl mb-5">
-          I code for the Web & Mobile.
+          Web and Mobile Developer
         </p>
         <p className="text-base md:text-lg mb-12 leading-8 text-gray-600 max-w-xl">
-          I am a software developer mainly focused on the <b>front-end</b> to create&nbsp;
-          <b>attractive and better apps</b> for both web and mobile. Apart from coding, I
-          like to play chess.
+          I&apos;m a passionate software developer from India, mainly focusing on&nbsp;
+          <b>front-end</b> to build <b>creative</b>, <b>attractive</b> and <b>better</b>
+          &nbsp; apps for web and mobile.
         </p>
         <div className="flex gap-x-5">
           <button className="w-fit font-medium text-white border-2 border-black bg-black cursor-pointer rounded-full transition-transform hover:text-black hover:bg-white active:translate-y-0.5">
--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
   redirects: async () => {
     return [
       {
