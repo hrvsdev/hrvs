b513857d05e5443e2bbed4a860a070230ee17d27
kingvyas <kumarvyasss@gmail.com>
2022-10-20 19:02:48 +0530

    changed hero text size

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -2,11 +2,11 @@ export default function HeroComp(): JSX.Element {
   return (
     <div className="flex justify-center sm:items-center h-screen py-14">
       <div className="flex flex-col max-w-screen-lg w-full">
-        <p className="text-lg sm:text-xl text-gray-500 pl-1 mb-1">Hi there, I&apos;m</p>
-        <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-2 sm:mb-4">
+        <p className="text-lg sm:text-xl text-gray-600 pl-1 mb-2">Hi there, I&apos;m</p>
+        <h1 className="text-5xl sm:text-6xl md:text-8xl font-bold mb-4 sm:mb-6 md:mb-8">
           Harsh Vyas
         </h1>
-        <p className="text-4xl sm:text-5xl md:text-6xl mb-5">
+        <p className="text-4xl sm:text-5xl md:text-7xl leading-snug mb-6">
           Web and Mobile Developer
         </p>
         <p className="text-base md:text-lg mb-12 leading-8 text-gray-600 max-w-xl">
