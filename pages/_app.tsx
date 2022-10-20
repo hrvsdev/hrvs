61b88b73c8fdf52f27606f504696e27279babe78
kingvyas <kumarvyasss@gmail.com>
2022-10-20 19:43:45 +0530

    added meta title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Harsh Vyas | Web & Mobile Dev</title>
+        <meta
+          name="description"
+          content="I'm Harsh, a passionate web and mobile developer who loves building better apps."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
