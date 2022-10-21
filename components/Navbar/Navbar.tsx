2babbf0637ddcad5827acd7362e52187be60ffaf
kingvyas <kumarvyasss@gmail.com>
2022-10-21 13:22:12 +0530

    added navbar bg

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Right from "./Right";
 
 export default function Navbar() {
   return (
-    <nav className="fixed flex justify-center items-center h-20 sm:h-24 px-6 border-b border-gray-100 w-full">
+    <nav className="fixed flex justify-center items-center h-20 sm:h-24 px-6 bg-white border-b border-gray-100 w-full">
       <div className="flex justify-between items-center pl-1 max-w-5xl w-full">
         <Left/>
         <Right/>
