da5b035c181ffdcb593b8107a70f5a66307e0e60
kingvyas <kumarvyasss@gmail.com>
2022-10-21 16:59:54 +0530

    added dark mode support

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../Navbar";
 import HeroComp from "./Hero";
+import Tech from "./Tech";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Navbar />
-      <main className="px-6">
+      <main className="px-6 dark:bg-slate-900">
         <HeroComp />
+        <Tech/>
       </main>
     </>
   );
---
+++ components/Home/Tech/Tech.tsx
@@ -0,0 +1,5 @@
+export default function Tech() {
+  return (
+    <section className="min-h-screen">Tech</section>
+  )
+}
