f7704cd2df2a4cf80b1e39d0bddfd4bdcc9c1601
kingvyas <kumarvyasss@gmail.com>
2022-10-26 19:48:04 +0530

    removed useless margin bottom in tech section

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -1,8 +1,6 @@
 export default function Projects() {
   return (
-    <section
-      id="tech"
-      className="flex justify-center bg-gradient-to-br from-sky-200 to-blue-200 py-10 px-6 mb-8">
+    <section className="flex justify-center bg-gradient-to-br from-purple-200 to-pink-200 py-10 px-6">
       <div className="max-w-5xl w-full"></div>
     </section>
   );
