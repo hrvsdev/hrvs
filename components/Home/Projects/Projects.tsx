d47906c21023a63ebd2ced8a0ad6bc6ed4fdc99b
kingvyas <kumarvyasss@gmail.com>
2022-10-26 20:06:17 +0530

    added project section bg with color palette

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -1,7 +1,11 @@
+import Heading from "./Heading";
+
 export default function Projects() {
   return (
-    <section className="flex justify-center bg-gradient-to-br from-purple-200 to-pink-200 py-10 px-6">
-      <div className="max-w-5xl w-full"></div>
+    <section className="flex justify-center bg-gradient-to-br from-sky-500 to-blue-500 py-10 px-6">
+      <div className="max-w-5xl w-full">
+        <Heading />
+      </div>
     </section>
   );
 }
