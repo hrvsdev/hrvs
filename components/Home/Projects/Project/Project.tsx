7d6d46e79f04bddc2f32db007ce31fb0f61ce673
kingvyas <kumarvyasss@gmail.com>
2022-10-26 21:01:33 +0530

    styling project UI

--- components/Home/Projects/Project/Project.tsx
+++ components/Home/Projects/Project/Project.tsx
@@ -1,5 +1,7 @@
 export default function Project() {
   return (
-    <div>Project</div>
+    <div className="py-4 px-8 shadow-xl rounded-xl bg-white/90 backdrop-blur">
+      <h1 className="text-2xl">Secured Text</h1>
+    </div>
   )
 }
--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -1,10 +1,12 @@
 import Heading from "./Heading";
+import Project from "./Project";
 
 export default function Projects() {
   return (
     <section className="flex justify-center bg-gradient-to-br from-sky-500 to-blue-500 py-10 px-6">
       <div className="max-w-5xl w-full">
         <Heading />
+        <Project />
       </div>
     </section>
   );
