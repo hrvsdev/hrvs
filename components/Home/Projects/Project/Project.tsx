0c77d7e66c8663581c85661e28e927191f789ab6
kingvyas <kumarvyasss@gmail.com>
2022-10-27 14:06:51 +0530

    added project image

--- components/Home/Projects/Project/Project.tsx
+++ components/Home/Projects/Project/Project.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
 export default function Project() {
   return (
-    <div className="py-4 px-8 shadow-xl rounded-xl bg-white/90 backdrop-blur">
-      <h1 className="text-2xl">Secured Text</h1>
+    <div className="flex flex-col md:flex-row border shadow-xl rounded-md bg-white/90 backdrop-blur">
+      <div className="md:w-1/2 aspect-video border-b md:border-b-0 md:border-r">
+        <img
+          alt="secured-text"
+          src="/images/projects/securedtext.png"
+          className="rounded-md"
+        />
+      </div>
+      <div className="md:w-1/2">
+    <h1 className="text-3xl font-semibold">Secured Text</h1>
+    <h1 className="text-3xl font-semibold">Secured Text</h1>
+    <h1 className="text-3xl font-semibold">Secured Text</h1>
+    <h1 className="text-3xl font-semibold">Secured Text</h1>
+      </div>
     </div>
-  )
+  );
 }
