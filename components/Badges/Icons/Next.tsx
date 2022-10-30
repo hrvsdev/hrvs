ea456e47fc971a83c2f4c7d569ef627f10f3271d
kingvyas <kumarvyasss@gmail.com>
2022-10-30 09:37:45 +0530

    added icon for badges

---
+++ components/Badges/Icons/Next.tsx
@@ -0,0 +1,29 @@
+import type { SVGProps } from "react";
+
+export function IconNext(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}>
+      <defs>
+        <linearGradient id="svgIDc" x1="55.633%" x2="83.228%" y1="56.385%" y2="96.08%">
+          <stop offset="0%" stopColor="#FFF"></stop>
+          <stop offset="100%" stopColor="#FFF" stopOpacity="0"></stop>
+        </linearGradient>
+        <linearGradient id="svgIDd" x1="50%" x2="49.953%" y1="0%" y2="73.438%">
+          <stop offset="0%" stopColor="#FFF"></stop>
+          <stop offset="100%" stopColor="#FFF" stopOpacity="0"></stop>
+        </linearGradient>
+        <circle id="svgIDa" cx="128" cy="128" r="128"></circle>
+      </defs>
+      <mask id="svgIDb" fill="#fff">
+        <use href="#svgIDa"></use>
+      </mask>
+      <g mask="url(#svgIDb)">
+        <circle cx="128" cy="128" r="128"></circle>
+        <path
+          fill="url(#svgIDc)"
+          d="M212.634 224.028L98.335 76.8H76.8v102.357h17.228V98.68L199.11 234.446a128.433 128.433 0 0 0 13.524-10.418Z"></path>
+        <path fill="url(#svgIDd)" d="M163.556 76.8h17.067v102.4h-17.067z"></path>
+      </g>
+    </svg>
+  );
+}
